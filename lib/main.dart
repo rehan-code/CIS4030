ff695821a41b5becf763c74d49ead877edaf67c5
Greg Shalay <gshalay@uoguelph.ca>
2022-03-14 17:34:15 -0400

    Firebase initialized, still need to find out how to retrieve and send data

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:my_games_tracker/view/pages/login_page.dart';
 import 'package:my_games_tracker/view/widgets/theme_provider.dart';
 import 'package:provider/provider.dart';
 import '/view/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
   ThemeProvider themeProvider = ThemeProvider();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp(themeProvider));
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _myGamesTrackerDB = Firebase.initializeApp();
   ThemeProvider themeProvider;
   MyApp(this.themeProvider);
 
@@ -24,9 +28,44 @@ class MyApp extends StatelessWidget {
           themeMode: themeProvider.themeMode,
           theme: MyThemes.lightTheme,
           darkTheme: MyThemes.darkTheme,
-          home: Scaffold(
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            body: LoginScreen(themeProvider),
+          home: FutureBuilder(
+            future: _myGamesTrackerDB,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print("Firebase Error: ${snapshot.error.toString()}");
+                return Text("Couldn't load Firebase Database!");
+              } else if (snapshot.hasData) {
+                print("FIREBASE INTIALIZED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                // FirebaseFirestore f = FirebaseFirestore.instance;
+
+                // CollectionReference users =
+                //     FirebaseFirestore.instance.collection('users');
+
+                // FutureBuilder(
+                //   future: users.doc('NcmeaiDE31ShNGUFBuq2').get(),
+                //   builder: (context, snapshot) {
+                //     if (snapshot.hasError) {
+                //       print("Error getting data...");
+                //     } else if (snapshot.connectionState ==
+                //         ConnectionState.done) {
+                //       Map<String, dynamic> d =
+                //           snapshot.data as Map<String, dynamic>;
+                //       print("GOT DATA BOIIIIISSSSSSSSS!!!!!!!!");
+                //     }
+                //     return Container();
+                //   },
+                // );
+
+                return Scaffold(
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  body: LoginScreen(themeProvider),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
           ),
         );
       },
